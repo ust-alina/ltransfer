0c4cdbf9fae37c1be80e0164aad44cad76cf5c0b
ust-alina <66687007+ust-alina@users.noreply.github.com>
2021-04-09 12:56:20 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
 
+      /*----Выбор языка-----*/
+
     $('.language_select').click(function(){
         $(this).toggleClass('open');
+        $('.language_select_block').toggleClass('open');
         })
         
         $('.language_select li').click(function(){
               $('.language_select li').removeClass('active');
               $(this).toggleClass('active');
         })
-
+      
+        /*----Выбор валюты-----*/
 
         $('.currency_select').click(function(){
             $(this).toggleClass('open');
+            $('.currency_select_block').toggleClass('open');
             })
             
             $('.currency_select li').click(function(){
@@ -19,4 +24,29 @@ $(document).ready(function(){
                   $(this).toggleClass('active');
             })
 
+            /*----Работа мобильного меню-----*/
+
+            $('.menu-burger__header').click(function() {
+                  $('.menu-burger__header').toggleClass('open-menu');
+                  $('.header_menu').toggleClass('open-menu');
+                  $('body').toggleClass('fixed-page');
+            });
+
+           
+
+});
+
+ /*-----Слайдер популярные маршруты в мобильной версии------*/
+ $(window).on('load resize', function() {
+ if ($(window).width() <= '480'){
+      $('.popular_slider').slick({
+            dots: false,
+            slidesToShow: 1,
+            speed: 1000,
+            infinite: false,
+            variableWidth: true
+      });
+      }  else {
+      $(".popular_slider").slick("unslick");
+      }
 });
